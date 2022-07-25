b11df9289966148be436cfa012508f48dfe267a9
Priyanka-Shahi <shahipriyan@gmail.com>
2022-07-25 19:28:31 +1000

    array map

--- index.js
+++ index.js
@@ -12,6 +12,24 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  // use the .map method on the tutorials to return a new array
-  return tutorials
+  let newT = [];
+  for(let i = 0; i < tutorials.length; i++) {
+    newT[i] = uppercase(tutorials[i]);
+  }
+  return newT;
+  }
+
+function uppercase(str)
+{
+  let array1 = str.split(' ');
+  let newarray1 = [];
+    
+  for(let x = 0; x < array1.length; x++){
+      newarray1.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
+  }
+  return newarray1.join(' ');
 }
+
+
+
+console.log(titleCased(tutorials));
